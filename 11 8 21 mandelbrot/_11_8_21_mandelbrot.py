306c6e24668e67be584bc352fd0c4c5618f9e2aa
Isrrael Chavez <junior_nachos@hotmail.com>
2021-11-10 08:42:30 -0700

    Added zoom

--- 11 8 21 mandelbrot/_11_8_21_mandelbrot.py
+++ 11 8 21 mandelbrot/_11_8_21_mandelbrot.py
@@ -25,45 +25,48 @@ def mandelbrot(c, zoomLevel,resolution):
 #in C++, this would be the start of main
 
 def run():
-    pygame.init()
-    t = -2 #lower bound for real axis
-    zoomLevel = 2
+    #pygame.init()
+    zoomLevel = 3
+    t = -2/zoomLevel #lower bound for real axis
+    offset = 0
     
-    while t<2: #upper bound for real (horizontal) axis
+    events=pygame.event.get()
+    for event in events:
+        if event.type == pygame.QUIT:
+            pygame.quit()
+    while t<2/(zoomLevel): #upper bound for real (horizontal) axis
         
         t+=.001 #make this number SMALLER to increase picture resolution
 
-        m = -2 #lower bound for imaginary axis
-        while m<2: #upper bound for imaginary (vertical) axis
-            events = pygame.event.get()
-
-            for event in events:
-                if event.type == pygame.QUIT:
-                    pygame.quit()
+        m = -2/zoomLevel #lower bound for imaginary axis
+        while m<2/(zoomLevel): #upper bound for imaginary (vertical) axis
+            
                     
             m+=.001 #make this number SMALLER to increase picture resolution
         
             c = complex(t, m) #create a complex number from iterators
-            num = mandelbrot(c, zoomLevel, t); #call the function
+            num = mandelbrot(c, zoomLevel, t) #call the function
 
             #these if statements are just to differentiate the colors more, not needed if you want black & white image
-            if num < 20 and (zoomLevel*t * 200 + 400) < 800 and (zoomLevel*t * 200 + 400) > 0 and (zoomLevel*m * 200 + 400) < 800 and (zoomLevel*m * 200 + 400) > 0:
-                screen.set_at((int(zoomLevel*t * 200 + 400), int(zoomLevel*m * 200 + 400)), (num*3, num*6, num*12))
-            elif num<40 and (zoomLevel*t * 200 + 400) < 800 and (zoomLevel*t * 200 + 400) > 0 and (zoomLevel*m * 200 + 400) < 800 and (zoomLevel*m * 200 + 400) > 0:
-               screen.set_at((int(zoomLevel*t * 200 + 400), int(zoomLevel*m * 200 + 400)), (num*2, num/2, num*4))
-            elif num is 80 and (zoomLevel*t * 200 + 400) < 800 and (zoomLevel*t * 200 + 400) > 0 and (zoomLevel*m * 200 + 400) < 800 and (zoomLevel*m * 200 + 400) > 0:
-               screen.set_at((int(zoomLevel*t * 200 + 400), int(zoomLevel*m * 200 + 400)), (2,255,255))
+            if num < 20:
+                screen.set_at(((int(zoomLevel * t * 200 + 400)+offset), (int(zoomLevel * m * 200 + 400))+offset), (num*6, num*12, num*12))
+            elif num<40:
+               screen.set_at(((int(zoomLevel * t * 200 + 400)+offset), (int(zoomLevel * m * 200 + 400))+offset), (num*2, num/2, num*4))
+            elif num is 80:
+               screen.set_at(((int(zoomLevel * t * 200 + 400)+offset), (int(zoomLevel * m * 200 + 400))+offset), (2,255,255))
             else:
-               screen.set_at((int(zoomLevel*t * 200 + 400), int(zoomLevel*m * 200 + 400)), (num*3, num/2, num*2))
+               screen.set_at(((int(zoomLevel * t * 200 + 400)+offset), (int(zoomLevel * m * 200 + 400))+offset), (num*3, num/2, num*2))
 
-            pygame.display.flip()#this actually puts the pixel on the screen
+        pygame.display.flip()#this actually puts the pixel on the screen
         
+
 t1 = threading.Thread(target=run)
 t2 = threading.Thread(target=run)
 #t3 = threading.Thread(target=run)
 #t4 = threading.Thread(target=run)
 t1.start()
 t2.start()
+pygame.time.wait(10000)
 #t3.start()
 #t4.start()
 #pygame.time.wait(10000)#pause to see the picture
